70766640ebbe21669d02e9a3e0960dc8129b5d0b
Juan Jose Garcia Laverde <juanjgl2007@hotmail.com>
2026-08-20 10:55:02 -0500

    feat: inicializar proyecto con estructura de carpetas y modelos

---
+++ app/src/main/java/com/example/prestamolabctma_2/model/SolicitudPrestamo.kt
@@ -0,0 +1,10 @@
+package com.example.prestamolabctma_2.model
+
+data class SolicitudPrestamo(
+    val id: Int,
+    val equipoId: Int,
+    val ambienteDestino: String,
+    val proposito: String,
+    val duracionHoras: Int,
+    val estado: EstadoSolicitud
+)
